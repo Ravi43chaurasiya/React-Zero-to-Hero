dc3eee9bdcbae89c6f8435afb9c0b3e0e1a68818
Ravi43chaurasiya <ravi8601150552@gmail.com>
2024-02-25 12:11:12 +0530

    Initial commit

---
+++ episode-01/app.js
@@ -0,0 +1,20 @@
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello world from React"
+// );
+
+// console.log(heading); // object
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I am h1 tag")
+  )
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
+// root.render(heading);
